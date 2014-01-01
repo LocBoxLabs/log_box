2d5e90eca851f911fdcce908d8c1776f736a704c
Jun Yoshida <yoshida.jun@gmail.com>
2014-01-01 11:57:36 -0800

    [add] log(), flush() and private methods

--- lib/log_box.rb
+++ lib/log_box.rb
@@ -1,5 +1,47 @@
+require "fluent-logger"
 require "log_box/version"
 
 module LogBox
-  # Your code goes here...
+  def self.log(obj, tag = :thread)
+    init_log_box_tag_if_not tag
+    log_box_location[tag] << { time: Time.now, log: obj }
+  end
+
+  def self.flush(tag = :thread)
+    result = {}.tap { |hash|
+      hash[:tag] = tag
+      hash[:logs] = log_box_location[tag]
+    }
+    flush_to_fluentd result
+    init_log_box_tag tag
+  end
+
+  private
+
+  def self.log_box_location
+    Thread.current[:_log_box]
+  end
+
+  def self.init_log_box_location
+    Thread.current[:_log_box] = {}  # key: tag, value: Array of Hash
+  end
+
+  def self.init_log_box_if_not
+    init_log_box_location if log_box_location.nil?
+  end
+
+  def self.init_log_box_tag_if_not(tag = :thread)
+    init_log_box_if_not
+    log_box_location[tag] ||= []
+  end
+
+  def self.init_log_box_tag(tag = :thread)
+    log_box_location[tag] = []
+  end
+
+  Logger = Fluent::Logger::FluentLogger.new(nil)
+
+  def self.flush_to_fluentd(result)
+    Logger.post 'log_box', result
+  end
 end
--- log_box.gemspec
+++ log_box.gemspec
@@ -8,8 +8,8 @@ Gem::Specification.new do |spec|
   spec.version       = LogBox::VERSION
   spec.authors       = ["Jun Yoshida"]
   spec.email         = ["yoshida.jun@gmail.com"]
-  spec.description   = %q{TODO: Write a gem description}
-  spec.summary       = %q{TODO: Write a gem summary}
+  spec.description   = %q{group Rails log by Web transaction or any other grouping}
+  spec.summary       = %q{separate Rails log per Web transaction}
   spec.homepage      = ""
   spec.license       = "MIT"
 
@@ -20,4 +20,6 @@ Gem::Specification.new do |spec|
 
   spec.add_development_dependency "bundler", "~> 1.3"
   spec.add_development_dependency "rake"
+
+  spec.add_dependency 'fluent-logger'
 end
