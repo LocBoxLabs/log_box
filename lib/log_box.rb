83b45f20ced3c2c59caa672711e8267d1bac97cc
Jun Yoshida <yoshida.jun@gmail.com>
2014-01-05 19:43:00 -0800

    [add] LogBox.configure: set default_tag and logger

--- lib/log_box.rb
+++ lib/log_box.rb
@@ -2,36 +2,90 @@ require "log_box/version"
 require "fluent-logger"
 require "pp"
 
-module LogBox
-  DEFAULT_TAG = :thread
+class Hash
+  def symbolize_keys
+    inject({}) do |options, (key, value)|
+      value = value.symbolize_keys if defined?(value.symbolize_keys)
+      options[(key.to_sym rescue key) || key] = value
+      options
+    end
+  end
+end
 
+module LogBox
+  class << self
+    attr_accessor :configuration
+  end
+
+  def self.configure
+    self.configuration ||= Configuration.new
+    yield(configuration)
+  end
+
+  class Configuration
+    attr_accessor :logger
+
+    def initialize
+      @logger = Fluent::Logger::ConsoleLogger.new(STDOUT)
+      @default_tag = :thread
+    end
+  end
+
+  # Following hash is stored into Thread.current[:_log_box]
+  # {
+  #  :new_tag =>
+  #  [{:time=>2014-01-01 15:38:15 -0800, :log=>"Hi-ho", :priority=>3}],
+  #
+  #  :thread=>
+  #   [{:time=>2014-01-01 15:38:21 -0800, :log=>"Hello"},
+  #    {:time=>2014-01-01 15:38:23 -0800, :log=>"Hello2"}]
+  # }
   def self.log(obj, options = {})
-    o = { tag: DEFAULT_TAG, time: current_time, log: obj }.merge(options)
+    o = { tag: self.configuration.default_tag,
+      time: current_time,
+      log: obj.is_a?(String) ? obj : obj.inspect
+    }.merge(options).symbolize_keys
+
+=begin
+    o = { tag: self.configuration.default_tag, time: current_time }.merge(options).symbolize_keys
+    if obj.is_a?(String)
+      o[:log] = obj
+    elsif obj.class < ActiveRecord::Base
+      o = o.merge(class: obj.class.to_s).merge(obj.attributes)
+    elsif obj.is_a?(Hash)
+      o.merge!(obj)
+    else
+      o[:log] = obj.inspect
+    end
+=end
     tag = o.delete :tag
     init_log_box_tag_if_not tag
     log_box[tag] << o
   end
 
-
-  # Following log will be stored into fluentd:
+  # Following log is stored into fluentd:
   # {
   #     "_id" : ObjectId("52c4a1f4e1eef37b9900001a"),
-  #     "tag" : "delayed_job",
+  #     "tag" : "thread",
   #     "logs" : [
   #         {
   #             "time" : "2014-01-01 15:16:43 -0800",
-  #             "log" : "Ho-ho",
-  #             "priority" : 3
+  #             "log" : "Hi-ho",
   #         }
   #     ],
   #     "time" : ISODate("2014-01-01T23:17:01.000Z")
   # }
   def self.flush(options = {})
-    o = { tag: DEFAULT_TAG }.merge(options)
+    o = { tag: self.configuration.default_tag }.merge(options).symbolize_keys
     tag = o[:tag]
     o[:logs] = log_box[tag]
     flush_to_fluentd o
-    delete_log_box_tag tag
+    discard tag
+  end
+
+  def self.discard(tag = nil)
+    tag ||= self.configuration.default_tag
+    log_box.delete tag
   end
 
   def self.display
@@ -41,6 +95,7 @@ module LogBox
   def self.log_box
     Thread.current[:_log_box]
   end
+
 
   private
 
@@ -56,22 +111,18 @@ module LogBox
     init_log_box if log_box.nil?
   end
 
-  def self.init_log_box_tag_if_not(tag = :thread)
+  def self.init_log_box_tag_if_not(tag = nil)
+    tag ||= self.configuration.default_tag
     init_log_box_if_not
     log_box[tag] ||= []
   end
 
-  def self.delete_log_box_tag(tag = :thread)
-    log_box.delete tag
-  end
-
-  def self.init_log_box_tag(tag = :thread)
+  def self.init_log_box_tag(tag = nil)
+    tag ||= self.configuration.default_tag
     log_box[tag] = []
   end
 
-  Logger = Fluent::Logger::FluentLogger.new(nil)
-
   def self.flush_to_fluentd(result)
-    Logger.post 'log_box', result
+    self.configuration.logger.post 'log_box', result
   end
 end
